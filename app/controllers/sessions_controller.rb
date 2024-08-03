b11992f10301656650842d5e1608f63dc53ee07c
yatoyun <kaneko.yuta0813@gmail.com>
2024-08-03 21:48:55 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user&.authenticate(session_params[:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,10 @@ module ActiveSupport
     fixtures :all
     include ApplicationHelper
 
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+
     # Add more helper methods to be used by all tests here...
   end
 end
