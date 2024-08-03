180bd2400b201607fc26a3a6f37f5255763d1f68
yatoyun <kaneko.yuta0813@gmail.com>
2024-08-03 17:31:03 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/about', to: "static_pages#about"
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
