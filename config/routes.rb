0d72a87345e1f2b5d2a29fcce0cc2d72687d2269
yatoyun <kaneko.yuta0813@gmail.com>
2024-08-08 22:41:27 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,15 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
